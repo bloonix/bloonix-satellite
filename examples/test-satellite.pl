30557e74cb28f8ddf73c2a893ecf8707b8a92fdc
Jonny Schulz <support@bloonix.de>
2015-06-22 14:18:20 +0200

    added a script to test the satellite

---
+++ examples/test-satellite.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Bloonix::IO::SIPC;
+use JSON;
+
+my $ip = $ARGV[0] || "127.0.0.1";
+my $authkey = $ARGV[1] || "abc";
+
+my $io = Bloonix::IO::SIPC->new(
+    peeraddr => $ip,
+    peerport => 5464,
+    use_ssl => "yes",
+    ssl_verify_mode => "none",
+    recv_timeout => 60
+);
+
+$io->connect;
+
+$io->send({
+    action => "exec",
+    authkey => $authkey,
+    data => {
+        command => "check-ping",
+        timeout => 30,
+        command_options => {
+            host => "127.0.0.1",
+            warning => "5000,33%",
+            timeout => "8000,66%"
+        }
+    }
+});
+
+my $res = $io->recv;
+
+print JSON->new->pretty->encode($res);
